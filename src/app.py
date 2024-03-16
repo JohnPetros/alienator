1418871b04421c7470644d6bc7eee44c0258f492
johnpetros <joaopedro.nc@outlook.com>
2024-03-16 20:58:41 -0300

    🔧 chore: init app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import views
+from views import init as init_views
 
-
-def create_app():
+if __name__ == "__main__":
     app = Flask(__name__)
 
-    views.init(app)
+    init_views(app)
 
-    return app
+    app.run(debug=True)
