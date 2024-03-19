56c5e323a88e81f13392b77b653e6c49e3e0db54
johnpetros <joaopedro.nc@outlook.com>
2024-03-19 17:27:47 -0300

    ✨ feat (utils): print error message of app error

--- src/utils/app_error.py
+++ src/utils/app_error.py
@@ -2,5 +2,6 @@ from flask import abort
 
 
 class AppError(BaseException):
-    def __init__(self, error: str, status_code: int = 500) -> None:
-        abort(status_code, error)
+    def __init__(self, message: str, status_code: int = 500) -> None:
+        print(message)
+        abort(status_code, message)
