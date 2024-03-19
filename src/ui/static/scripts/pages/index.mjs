afa6e76c9a51455fae7b45c731f24930cc7a0102
johnpetros <joaopedro.nc@outlook.com>
2024-03-19 18:24:04 -0300

    🗃️ file: remove useless js files

--- src/ui/static/scripts/pages/index.mjs
+++ src/ui/static/scripts/pages/index.mjs
@@ -1,15 +1,6 @@
-import { Form } from '../components/form.mjs'
 import { Hyperscript } from '../components/hyperscript.mjs'
-import { Modal } from '../components/modal.mjs'
 
 window.addEventListener('load', () => {
-  const modal = new Modal('modal')
-
-  const gameForm = new Form('[data-form="game-form"]')
-  const modalGameForm = new Form('#modal [data-form="game-form"]')
-
-  gameForm.onSubmit = () => modal.close()
-  modalGameForm.onSubmit = () => modal.close()
 
   const hyperscript = new Hyperscript()
 
