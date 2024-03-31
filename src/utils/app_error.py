961746ed7a527997722b7806e5bbd8e54e46ebc5
johnpetros <joaopedro.nc@outlook.com>
2024-03-31 11:32:35 -0300

    🧰 utils: abort server when it is needed

--- src/utils/app_error.py
+++ src/utils/app_error.py
@@ -2,6 +2,10 @@ from flask import abort
 
 
 class AppError(BaseException):
-    def __init__(self, message: str, status_code: int = 500) -> None:
+    def __init__(
+        self, message: str, status_code: int = 500, should_abort: bool = False
+    ) -> None:
         print(message)
-        abort(status_code, message)
+
+        if should_abort:
+            abort(status_code, message)
