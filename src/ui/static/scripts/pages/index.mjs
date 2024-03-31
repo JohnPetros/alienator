820d9ba183b3cb64010f5b833bc524b362352389
johnpetros <joaopedro.nc@outlook.com>
2024-03-31 11:33:28 -0300

    🪄 formatting: js files

--- src/ui/static/scripts/pages/index.mjs
+++ src/ui/static/scripts/pages/index.mjs
@@ -1,11 +1,9 @@
 import { Hyperscript } from '../components/hyperscript.mjs'
 
 window.addEventListener('load', () => {
-
   const hyperscript = new Hyperscript()
 
   document.body.addEventListener('htmx:afterOnLoad', (event) => {
     hyperscript.precessElement(event.detail.elt)
   })
 })
-
