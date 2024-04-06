cfe34d0095a1c4023137c4279f2cd3dda6d4a271
johnpetros <joaopedro.nc@outlook.com>
2024-04-05 23:03:42 -0300

    ⚙️ config: remove use tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
-  content: [
-    './src/ui/templates/**/*.html',
-    './src/ui/static/src/**/*.js',
-    'node_modules/preline/dist/*.js',
-  ],
+  content: ['./src/ui/templates/**/*.html', './src/ui/static/src/**/*.js'],
   theme: {
     extend: {
       backgroundSize: {
         'mobile-size': '40rem 100%',
       },
-    }
+    },
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require('tailwindcss-animated'),
-  ],
+  plugins: [require('flowbite/plugin'), require('tailwindcss-animated')],
 }
